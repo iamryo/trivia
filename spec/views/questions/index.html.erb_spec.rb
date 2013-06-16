34be63ab49f02c4416a04b6917f61b7b489292d9
Ryan Clark <rclark.sf@gmail.com>
2013-06-16 11:14:47 -0700

    testing setup

--- spec/views/questions/index.html.erb_spec.rb
+++ spec/views/questions/index.html.erb_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
 describe "questions/index" do
-  before(:each) do
-    assign(:questions, [
-      stub_model(Question),
-      stub_model(Question)
-    ])
+  describe "questions/index" do
+    before(:each) do
+      assign(:questions, [
+        FactoryGirl.create(:question, :body => "What is your name?"),
+        FactoryGirl.create(:question, :body => "What is the date?")
+      ])
+    end
   end
 
   it "renders a list of questions" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_response :success
   end
 end
