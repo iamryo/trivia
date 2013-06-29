167f5e2f408a23d08362c3f89df7deae9200c133
Ryan Clark <rclark.sf@gmail.com>
2013-06-28 17:59:18 -0700

    More answers_controller specs

--- spec/integration/view_answers_index_spec.rb
+++ spec/integration/view_answers_index_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-feature 'View the homepage' do
+feature 'View answers index page' do
   scenario 'user sees relevant information' do
-    visit root_path
-    expect(page).to have_css 'h1', text: 'Questions'
+    visit answers_path
+    expect(page).to have_css 'h1', text: 'Answers'
     expect(page).to have_css  'a.brand', text: 'Trivia'
   end
 end
---
+++ spec/integration/view_answers_show_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'View answer show page' do
+  scenario 'user sees relevant information' do
+    @answer = FactoryGirl.create(:answer)
+    visit answer_path(@answer)
+    expect(page).to have_css 'h1', text: 'Answer'
+    expect(page).to have_css  'a.brand', text: 'Trivia'
+  end
+end
