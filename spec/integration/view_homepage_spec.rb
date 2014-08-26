2959a678cf3d6da301e412294b1bbb680b7094f2
Ryan Clark <rclark.sf@gmail.com>
2014-08-26 16:41:31 -0700

    Comment out a bunch of tests to start over

--- spec/integration/view_homepage_spec.rb
+++ spec/integration/view_homepage_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 feature 'View the homepage' do
-  scenario 'user sees relevant information' do
-    visit root_path
-    expect(page).to have_css 'h1', text: 'Questions'
-    expect(page).to have_css  'a.brand', text: 'Trivia'
-  end
+  # scenario 'user sees relevant information' do
+  #   visit root_path
+  #   expect(page).to have_css 'h1', text: 'Questions'
+  #   expect(page).to have_css  'a.brand', text: 'Trivia'
+  # end
 end
