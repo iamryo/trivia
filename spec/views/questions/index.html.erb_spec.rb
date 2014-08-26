2796dbd239c0b278e04a9c38fd7cb02fae999083
Ryan Clark <rclark.sf@gmail.com>
2014-08-26 16:41:31 -0700

    Comment out a bunch of tests to start over

--- spec/views/questions/index.html.erb_spec.rb
+++ spec/views/questions/index.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe "questions/index" do
-  before(:each) do
-    assign(:questions, [
-      FactoryGirl.create(:question, :body => "What is your name?"),
-      FactoryGirl.create(:question, :body => "What is the date?")
-    ])
-  end
-
-  it "renders a list of questions" do
-    render
-  end
+  # before(:each) do
+  #   assign(:questions, [
+  #     FactoryGirl.create(:question, :body => "What is your name?"),
+  #     FactoryGirl.create(:question, :body => "What is the date?")
+  #   ])
+  # end
+  #
+  # it "renders a list of questions" do
+  #   render
+  # end
 end
---
+++ spec/views/questions/show.html.haml_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "questions/show" do
+  it 
+end
